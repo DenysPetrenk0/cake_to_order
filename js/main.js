901963b48a7e626987e2053ec12045d91711d21f
Denys Petrenko <petrenko@dbosoft.com.ua>
2025-02-10 17:43:07 +0200

    #6 added  gallery styles and logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 import {initSmoothScroll} from "./modules/scrolling.js";
+import {initModal} from "./modules/modal_gallery.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 	initSmoothScroll()
-})
+
+	const openModal = initModal();
+	const basePath = "./assets/img/gallery/";
+
+	document.querySelectorAll(".card").forEach((card) => {
+		card.addEventListener("click", () => {
+			let images = JSON.parse(card.getAttribute("data-images") || "[]");
+			images = images.map(img => basePath + img);
+			if (images.length > 0) {
+				openModal(images, 0);
+			}
+		});
+	});
+});
