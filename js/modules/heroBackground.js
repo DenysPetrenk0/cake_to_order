6ef5c50a760c8aa8cbf0ac59df6a469616ab0309
Denys Petrenko <petrenko@dbosoft.com.ua>
2025-03-07 01:34:40 +0200

    #23 added response

---
+++ js/modules/heroBackground.js
@@ -0,0 +1,31 @@
+const images = ["./assets/img/hero/bg.jpg", "./assets/img/hero/bg2.jpg", "./assets/img/hero/bg3.jpg", "./assets/img/hero/bg4.jpg"];
+let index = 0;
+const bgContainer = document.querySelector("#AboutIntro");
+
+export function changeBackground() {
+	if (window.innerWidth > 768) return;
+
+	const nextIndex = (index + 1) % images.length;
+	const nextImage = images[nextIndex];
+
+	bgContainer.style.setProperty("--prev-image", `url(${images[index]})`);
+	bgContainer.style.backgroundImage = `url(${nextImage})`;
+
+	bgContainer.classList.add("fade");
+
+	setTimeout(() => {
+		bgContainer.classList.remove("fade");
+		index = nextIndex;
+	}, 1000);
+}
+
+export function startBackgroundChange() {
+	if (window.innerWidth <= 1200) {
+		if (!bgContainer.dataset.interval) {
+			bgContainer.dataset.interval = setInterval(changeBackground, 4000);
+		}
+	} else {
+		clearInterval(bgContainer.dataset.interval);
+		bgContainer.dataset.interval = null;
+	}
+}
---
+++ js/modules/modalHeaderMenu.js
@@ -0,0 +1,63 @@
+export const initModalHeaderMenu = () => {
+	const backdrop = document.querySelector(".backdrop");
+	backdrop.innerHTML = `
+		<a href="#" class="modal__close">
+			<svg class="modal__icon">
+				<use href="./assets/icon/cross.svg#cross"></use>
+			</svg>
+		</a>
+		<ul class="modal__nav">
+                <li class="modal__item">
+                    <a href="#WhatIOffer" class="modal__link">
+                        пропонуємо
+                    </a>
+                </li>
+                <li class="modal__item">
+                    <a href="#AboutMe" class="modal__link">
+                        автор
+                    </a>
+                </li>
+                <li class="modal__item">
+                    <a href="#Gallery" class="modal__link">
+                        роботи
+                    </a>
+                </li>
+                <li class="modal__item">
+                    <a href="#Testimonials" class="modal__link">
+                        відгуки
+                    </a>
+                </li>
+                <li class="modal__item">
+                    <a href="#OrderProcess" class="modal__link">
+                        замовити
+                    </a>
+                </li>
+                <li class="modal__item">
+                    <a href="#Contact" class="modal__link">
+                        контакти
+                    </a>
+                </li>
+            </ul>
+	`;
+
+	const closeMenu = () => {
+		backdrop.classList.add("is-hidden");
+		document.querySelector("#menuBtn").style.display = "block";
+		backdrop.innerHTML = "";
+	};
+
+	document.querySelector("#menuBtn").style.display = "none";
+	backdrop.classList.remove("is-hidden");
+	backdrop.classList.add("modal__menu");
+
+	backdrop.querySelector(".modal__close").addEventListener("click", (event) => {
+		event.preventDefault();
+		closeMenu();
+	});
+
+	backdrop.querySelectorAll(".modal__link").forEach(link => {
+		link.addEventListener("click", (event) => {
+			closeMenu();
+		});
+	});
+};
