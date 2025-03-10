aac2e0eef84a52e69223cfc365ef3e5b132ec98a
Denys Petrenko <petrenko@dbosoft.com.ua>
2025-03-10 03:16:13 +0200

    #25 added response final

--- js/modules/modalGallery.js
+++ js/modules/modalGallery.js
@@ -3,6 +3,8 @@ export const initModalGallery = () => {
 
 	let images = [];
 	let currentIndex = 0;
+	let touchStartX = 0;
+	let touchEndX = 0;
 
 	function openModal(imgArray, index) {
 		images = imgArray;
@@ -14,8 +16,10 @@ export const initModalGallery = () => {
 					<use href="./assets/icon/cross.svg#cross"></use>
 				</svg>
 			</a>
-			<div class="modal">
-				<img class="modal__image gallery__img" src="${images[currentIndex]}" alt="Gallery Image">
+			<div class="modal modal__gallery">
+			<div class="modal__gallery__img__box">
+				<img class="modal__image gallery__img" src="${images[currentIndex]}" alt="Gallery Image">			
+			</div>
 				<a href="#" class="modal__prev">
 					<svg class="modal__icon">
 						<use href="./assets/icon/modalArrow.svg#modal-arrow"></use>
@@ -31,11 +35,11 @@ export const initModalGallery = () => {
 
 		backDrop.classList.remove("is-hidden");
 
-		// Отримуємо нові елементи
 		const modalImage = backDrop.querySelector(".modal__image");
 		const closeButton = backDrop.querySelector(".modal__close");
 		const prevButton = backDrop.querySelector(".modal__prev");
 		const nextButton = backDrop.querySelector(".modal__next");
+		const galleryImgBox = document.querySelector(".modal__gallery__img__box");
 
 		function closeModal(event) {
 			event.preventDefault();
@@ -55,10 +59,30 @@ export const initModalGallery = () => {
 			modalImage.src = images[currentIndex];
 		}
 
+		function handleTouchStart(event) {
+			touchStartX = event.touches[0].clientX;
+		}
+
+		function handleTouchEnd(event) {
+			touchEndX = event.changedTouches[0].clientX;
+			handleSwipe(event);
+		}
+
+		function handleSwipe(event) {
+			const swipeThreshold = 50;
+			if (touchStartX - touchEndX > swipeThreshold) {
+				showNextImage(event);
+			} else if (touchEndX - touchStartX > swipeThreshold) {
+				showPrevImage(event);
+			}
+		}
+
 		closeButton.addEventListener("click", closeModal);
 		prevButton.addEventListener("click", showPrevImage);
 		nextButton.addEventListener("click", showNextImage);
 		modalImage.addEventListener("click", showNextImage);
+		modalImage.addEventListener("touchstart", handleTouchStart);
+		modalImage.addEventListener("touchend", handleTouchEnd);
 
 		backDrop.addEventListener("click", (e) => {
 			if (e.target === backDrop) {
